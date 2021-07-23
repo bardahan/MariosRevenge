8d1b90bea7226c8a2560e00e7b5f723d2d4bcee9
idoattaly <ido@apiiro.com>
2021-07-24 02:13:56 +0300

    basic enemies

--- Assets/Scripts/BasicEnemyScript.cs
+++ Assets/Scripts/BasicEnemyScript.cs
@@ -6,49 +6,142 @@ public class BasicEnemyScript : MonoBehaviour
 {
     public Transform[] ways;
     public Animator animator;
-    public int speed;
+    public float speed;
     int index;
     bool isWalking;
+    bool isAttackInProgress;
+    Transform player;
+    bool isGrounded;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isAttacking;
+    public float runSpeed;
+    public float walkSpeed;
+    public float life;
+    public float singleAttackDuration;
+    public float attackDistance;
+    public int damage;
+    public int bulletDamage;
 
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
-        speed = 1;
+        speed = runSpeed;
         isWalking = true;
+        isAttackInProgress = false;
+        isAttacking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform current = ways[index];
-        Vector3 dir = current.position - transform.position;
+        float horisontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        Vector3 direction = new Vector3(horisontal, 0f, vertical).normalized;
+        gameObject.GetComponent<CharacterController>().Move(velocity * Time.deltaTime);
 
-        if (dir.magnitude < 1)
+        if (!isAttacking && life > 0)
         {
-            index = Random.Range(0, 3);
-            wait();
+            if (!isAttackInProgress)
+            {
+                Transform current = ways[index];
+                Vector3 dir = current.position - transform.position;
+
+                if (dir.magnitude < 1)
+                {
+                    index = Random.Range(0, 3);
+                    wait();
+                }
+
+                if (isWalking)
+                {
+                    SelfRotation(current);
+                    gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                else if (Random.Range(0, 60 * 3) == 0)
+                {
+                    Walk();
+                }
+            }
+            else
+            {
+                Vector3 dir = player.position - transform.position;
+
+                if (dir.magnitude < attackDistance)
+                {
+                    StartCoroutine(Attack());
+                }
+
+                if (isWalking)
+                {
+                    SelfRotation(player);
+                    gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                else
+                {
+                    PerformAttack();
+                }
+            }
         }
 
-        if (isWalking)
+        if (life <= 0)
         {
-            SelfRotation(current);
-            gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            StartCoroutine(Die());
         }
-        else if (Random.Range(0, 60 * 3) == 0)
-        {
-            Walk();
-        }
+    }
+
+    private IEnumerator Die()
+    {
+        animator.SetBool("isDie", true);
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator Attack()
+    {
+        isWalking = false;
+        isAttacking = true;
+        animator.SetBool("isAttacking", true);
+
+        yield return new WaitForSeconds(singleAttackDuration);
+
+        isWalking = true;
+        isAttacking = false;
+        animator.SetBool("isAttacking", false);
+    }
+
+    private void PerformAttack()
+    {
+        speed = runSpeed;
+        isWalking = true;
+        animator.SetBool("isWaiting", false);
+    }
+
+    public void StartAttack(Transform other)
+    {
+        player = other;
+        isAttackInProgress = true;
+        PerformAttack();
     }
 
     void SelfRotation(Transform target)
     {
-        transform.LookAt(target.position);
+        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
     }
 
     void Walk()
     {
-        speed = 1;
+        speed = walkSpeed;
         isWalking = true;
         animator.SetBool("isWaiting", false);
     }
@@ -57,5 +150,13 @@ public class BasicEnemyScript : MonoBehaviour
     {
         isWalking = false;
         animator.SetBool("isWaiting", true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            life = 0;
+        }
     }
 }
