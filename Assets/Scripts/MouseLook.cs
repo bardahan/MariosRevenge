666a63007a3f00f9aac832f3660e64614707234f
bar dahan <bbarr100@gmail.com>
2021-07-23 15:59:46 +0300

    .

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,10 +8,12 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerBody;
 
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -20,6 +22,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -50f, 0f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, -90f, 180f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
