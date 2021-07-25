fc5e104db165c2b264102585b2bf693465b3192c
idoattaly <ido@apiiro.com>
2021-07-25 21:39:55 +0300

    boss scene final

--- Assets/Scripts/BlockerScript.cs
+++ Assets/Scripts/BlockerScript.cs
@@ -13,12 +13,10 @@ public class BlockerScript : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             // Calculate Angle Between the collision point and the player
-            Vector3 dir = col.contacts[0].point - transform.position;
-            // We then get the opposite (-Vector3) and normalize it
-            dir = -dir.normalized;
-            // And finally we add force in the direction of dir and multiply it by force. 
-            // This will push back the player
-            col.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
+            Vector3 moveDir = col.contacts[0].point - transform.position;
+            gameObject.GetComponent<Rigidbody>().AddForce(moveDir * force);
+
+            col.gameObject.GetComponent<PlayerHealthScript>().TakeDamege(20);
         }
     }
 }
