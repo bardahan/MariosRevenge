b6c6b47539ac3e2d2fd572b0f873352bf1cfb084
bar dahan <bbarr100@gmail.com>
2021-07-27 16:36:52 +0300

    .

---
+++ Assets/CinemachinSwicher.cs
@@ -0,0 +1,14 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEn
+
+//public class CinemachinSwicher : MonoBehaviour
+//{
+//    private InputAction 
+
+//    private void OnEnable()
+//    {
+        
+//    }
+//}
